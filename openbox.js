817a2c0069348740fdbc53e3681ce77c94662cdf
gilkur23 <122338710+gilkur23@users.noreply.github.com>
2024-10-01 22:35:39 +0700

    Update openbox.js

--- openbox.js
+++ openbox.js
@@ -133,17 +133,26 @@ async function openAllBoxes(privateKey) {
 }
 
 (async () => {
-  const results = [];
+  const successfulClaims = [];
+  const failedClaims = [];
+
   try {
     for (const privateKey of PRIVATE_KEYS) {
       const result = await openAllBoxes(privateKey);
-      results.push(result);
+      if (result.includes('Berhasil')) {
+        successfulClaims.push(result);
+      } else {
+        failedClaims.push(result);
+      }
     }
 
-    const summaryMessage = `Ringkasan Box:\n${results.join('\n')}`;
-    console.log(summaryMessage);
+    const totalSuccessful = successfulClaims.length;
+    const totalFailed = failedClaims.length;
 
-    await sendTelegramMessage(summaryMessage);
+    const summaryMessage = `*Buka Semua Box*\nSukses: ${totalSuccessful} Akun\nGagal: ${totalFailed} Akun\n\n`;
+    fs.writeFileSync('summary_openbox.json', JSON.stringify({ summaryMessage }));
+    console.log(summaryMessage.green);
+
   } catch (error) {
     console.log(`Terjadi kesalahan: ${error.message}`.red);
   } finally {
